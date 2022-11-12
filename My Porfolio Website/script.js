401592dc3104bd5aa07ce5569d95071c8c090878
Avazbek <75464019+Avaz-tech@users.noreply.github.com>
2022-11-12 19:51:57 +0900

    Update script.js

--- My Porfolio Website/script.js
+++ My Porfolio Website/script.js
@@ -19,4 +19,4 @@ const sr = ScrollReveal({
 
 sr.reveal('.home-text',{delay:200, origin: 'top'} )
 sr.reveal('.home-img',{delay:400, origin: 'top'} )
-sr.reveal('.about, .services, .cta, .resume, contact, copyright',{delay:200, origin: 'top'} )
+sr.reveal('.about, .services, .cta, .resume, .contact, .copyright',{delay:200, origin: 'top'} )
